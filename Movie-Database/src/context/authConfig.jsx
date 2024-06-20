c263abbf175181b0ae9f08358a162730f7f0f46a
Stefan Janjić <jsteffan@gmail.com>
2024-06-20 16:10:38 +0200

    Firebase Auth setup

---
+++ Movie-Database/src/context/authConfig.jsx
@@ -0,0 +1,36 @@
+import { createContext, useContext, useState, useEffect } from 'react';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth';
+import { auth, db } from '../api/firebase';
+
+const AuthContext = createContext();
+
+export function AuthContextProvider({ children }) {
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  function signUp(email, password) {
+    createUserWithEmailAndPassword(auth, email, password);
+  }
+
+  function logIn(email, password) {
+    signInWithEmailAndPassword(auth, email, password);
+  }
+
+  function logOut() {
+    return signOut(auth);
+  }
+
+  return <AuthContext.Provider value={{ user, signUp, logIn, logOut }}>{children}</AuthContext.Provider>;
+}
+
+export function UserAuth() {
+  return useContext(AuthContext);
+}
