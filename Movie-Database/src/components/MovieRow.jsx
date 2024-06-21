e1baba4b020a58b2b976802f90bab6c374e8f89b
Stefan Janjić <jsteffan@gmail.com>
2024-06-21 09:26:09 +0200

    Watchlist setup

--- Movie-Database/src/components/MovieRow.jsx
+++ Movie-Database/src/components/MovieRow.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import MovieItem from './MovieItem';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
 const MovieRow = ({ title, url }) => {
   const [movies, setMovies] = useState([]);
+  const rowId = Math.floor(Math.random() * 10);
 
   useEffect(() => {
     axios.get(url).then((response) => {
@@ -11,17 +13,31 @@ const MovieRow = ({ title, url }) => {
     });
   }, [url]);
 
+  const slide = (offset) => {
+    const slider = document.getElementById('slider' + rowId);
+    slider.scrollLeft = slider.scrollLeft + offset;
+  };
+
   return (
     <>
-      <div>
-        <h2 className="capitalize md:text-3xl p-4 font-bold text-red-600">{title}</h2>
-        <div id={`slider`} className="relative flex items-center">
-          <div className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide">
-            {movies.map((movie) => (
-              <MovieItem key={movie.id} movie={movie} />
-            ))}
-          </div>
+      <h2 className="capitalize md:text-3xl p-4 font-bold text-red-600">{title}</h2>
+
+      <div className="relative flex items-center group">
+        <MdChevronLeft
+          onClick={() => slide(-350)}
+          size={40}
+          className="absolute rounded-full bg-white left-4 opacity-80 z-10 hidden text-red-600 cursor-pointer group-hover:block"
+        />
+        <div id={`slider` + rowId} className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide">
+          {movies.map((movie) => (
+            <MovieItem key={movie.id} movie={movie} />
+          ))}
         </div>
+        <MdChevronRight
+          onClick={() => slide(350)}
+          size={40}
+          className="absolute rounded-full bg-white right-4 opacity-80 z-10 hidden text-red-600 cursor-pointer group-hover:block"
+        />
       </div>
     </>
   );
--- Movie-Database/src/pages/Watchlist.jsx
+++ Movie-Database/src/pages/Watchlist.jsx
@@ -1,7 +1,88 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import { UserAuth } from '../context/AuthConfig';
+import { db } from '../api/firebase';
+import img from '../assets/img/wall.jpg';
+
+import { doc, onSnapshot, updateDoc, arrayRemove } from 'firebase/firestore';
+import { imageUrl } from '../api/apiConfig';
+import { AiFillCloseCircle } from 'react-icons/ai';
 
 const Watchlist = () => {
-  return <div>Watchlist</div>;
+  const [movies, setMovies] = useState([]);
+  const { user } = UserAuth();
+
+  useEffect(() => {
+    if (user) {
+      onSnapshot(doc(db, 'users', `${user.email}`), (doc) => {
+        if (doc.data()) setMovies(doc.data().savedShows);
+      });
+    }
+  }, [user, user.email]);
+
+  const slide = (offset) => {
+    const slider = document.getElementById('slider');
+    slider.scrollLeft = slider.scrollLeft + offset;
+  };
+
+  const handleRemove = async (movie) => {
+    const userDoc = doc(db, 'users', user.email);
+
+    await updateDoc(userDoc, {
+      savedShows: arrayRemove(movie),
+    });
+  };
+
+  return (
+    <>
+      <div>
+        <div>
+          <img className="w-full h-[700px] object-cover block " src={img} alt="/" />
+
+          <div className="bg-black/50 fixed top-0 left-0 w-full h-[700px]" />
+          <p className="text-4xl text-red-600 absolute top-[30%] p-4 md:p-8 ">{user.email}</p>
+        </div>
+        {/* movie row */}
+
+        <h2 className="capitalize md:text-3xl p-4 font-bold text-red-600">Fav Shows</h2>
+
+        <div className="relative flex items-center group">
+          <MdChevronLeft
+            onClick={() => slide(-350)}
+            size={40}
+            className="absolute rounded-full bg-white left-4 opacity-80 z-10 hidden text-red-600 cursor-pointer group-hover:block"
+          />
+          <div id={`slider`} className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide">
+            {movies.map((movie) => (
+              <div
+                key={movie.id}
+                className="relative w-[160px] sm:w-[200px] md:w-[240px] lg:w-[350px]  inline-block  cursor-pointer m-2 overlow-hidden"
+              >
+                <img
+                  className="w-full h-40 block object-cover object-top rounded-lg"
+                  src={imageUrl(movie.backdrop_path ?? movie.poster_path, 'w500')}
+                  alt={movie.title}
+                />
+
+                <div className="absolute top-0 left-0 w-full h-40 bg-black/70 opacity-0 hover:opacity-100">
+                  <p className="whitespace text-xs md:text-base  flex justify-center items-center h-full font-bold">{movie.title}</p>
+
+                  <p>
+                    <AiFillCloseCircle size={30} className="absolute top-4 right-4 text-red-600" onClick={() => handleRemove(movie)} />
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+          <MdChevronRight
+            onClick={() => slide(350)}
+            size={40}
+            className="absolute rounded-full bg-white right-4 opacity-80 z-10 hidden text-red-600 cursor-pointer group-hover:block"
+          />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Watchlist;
