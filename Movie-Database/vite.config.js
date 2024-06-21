fb1d0e24ca1e592773c9e07d6c755868c6535937
Stefan Janjić <jsteffan@gmail.com>
2024-06-21 12:28:58 +0200

    Vite update

--- Movie-Database/vite.config.js
+++ Movie-Database/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/StefanJDev/Movie-Database.git',
+  base: 'https://github.com/StefanJDev/Movie-Database/dist/',
   server: {
     port: 3000,
   },
